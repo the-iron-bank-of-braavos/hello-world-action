6327086f11cc1c069781a6f165338bc66ae891e0
David Hernández Martín <belcebus@gmail.com>
2021-11-11 21:06:44 +0100

    wip

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const core = require('@actions/core');
-
 async function run() {
-  try {
-
-    core.info("Hello world v1.0.0");
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    console.log("Hello world v1.0.0");
 }
 
 run();
